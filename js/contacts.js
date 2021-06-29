820af9096c2e10839c03bff6ac7d1c1a0ee936dd
Synthia Kageni <synthiakageni@gmail.com>
2021-06-29 16:29:39 +0300

    add contacts,location and feedback form

---
+++ js/contacts.js
@@ -0,0 +1,5 @@
+function reply() {
+    let name = document.getElementById("name").value;
+    let output =  "Hello " + name + ", we have received your feedback,thankyou for reaching out to us.";
+    alert(output);
+  }
