277015dbc6cbb42ffd8316cfbed5c2e6f74e6650
Francis Kinyae <fmkinyae@gmail.com>
2021-06-29 16:36:17 +0300

    add recipient form js

---
+++ js/recipient.js
@@ -0,0 +1,14 @@
+function receive(){
+    
+    let name = document.getElementById("firstname");
+
+    alert(name + ",Request received,Processing Process is now underway..we will respond in 5minutes..");
+
+};
+
+$().ready(function(event){
+    event.preventDefault();
+
+    
+
+});
