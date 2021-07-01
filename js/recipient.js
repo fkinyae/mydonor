cfe5405f64f7a22d1d60b2a1d50bd282afb6fa81
Francis Kinyae <fmkinyae@gmail.com>
2021-07-01 21:30:34 +0300

    add js for modal

--- js/recipient.js
+++ js/recipient.js
@@ -1,17 +1,43 @@
 $().ready(function(){
 
+
+    $('#recipient').hide();
+     $('#blood_donor').show();
+        $('#regrets').hide();
+
+            $("#myBtn").click(function(){
+                    $("#myModal").modal();
+            });
+     
+ 
     $('#btn-submit').click(function(event){
         event.preventDefault();
 
 
         let name = $('#firstname').val();
-        let donation = $()
-        let delivery = $('input[name="delivery"]:checked').val(); //"home" or "pickup";
+        let donation_type = $('input[name="donation"]:checked').val();
+        let age = $('#age').val();
+
 
 
         alert("Hello " + name + ", We have received your request, the processing is now underway.Please be patient.");
 
-        alert("We are pleased to inform you that,your request has been accepted. Your donor is code ....., please click to see his details");
+        alert("We are pleased to inform you that,your request has been accepted. please click to see your donors details");
+
+        switch (donation_type) {
+            case donation_type:
+                if (donation_type == "blood" && age <= 30) {
+                    $('#blood_donor').show();
+                    
+                } else {
+                    let don_msg = "We are sorry to inform you that, We currently do not have a " +donation_type + " donor. However, We are in the process of receiving more donors." + "Please be patient, Once we get a " + donation_type + " donor onboard, We will contact you immediately, We also advice you to possibly look for other sources."  
+                    $('#regret').html(don_msg);
+
+                    $('#regrets').show();
+
+            } 
+                break;
+        }
 
         $('#recipient').hide();
 
