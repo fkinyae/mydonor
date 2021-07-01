789ec6f043914797754d6819e889318d7299871d
Francis Kinyae <fmkinyae@gmail.com>
2021-07-01 23:53:11 +0300

    add switch case for selecting donors

--- js/recipient.js
+++ js/recipient.js
@@ -1,9 +1,15 @@
 $().ready(function(){
 
+/*      $('#recipient').hide(); 
+ */     $('#blood_donor').hide();
+     $('#blood_donor2').hide();
+     $('#blood_donor3').hide();
+     $('#send_msg').hide();
+     $('#regrets').hide();
 
-    $('#recipient').hide();
-     $('#blood_donor').show();
-        $('#regrets').hide();
+
+    
+    
 
             $("#myBtn").click(function(){
                     $("#myModal").modal();
@@ -31,16 +37,32 @@ $().ready(function(){
 
         switch (donation_type) {
             case donation_type:
-                if (donation_type == "blood" && age <= 30) {
+                if (donation_type === "blood" && age <= 25) {
                     $('#blood_donor').show();
+                    $('#send_msg').show();
                     
-                } else {
-                    let don_msg = "We are sorry to inform you that, We currently do not have a " +donation_type + " donor. However, We are in the process of receiving more donors." + "Please be patient, Once we get a " + donation_type + " donor onboard, We will contact you immediately, We also advice you to possibly look for other sources."  
-                    $('#regret').html(don_msg);
+                } else if (age > 25 && age <= 40 && donation_type === "blood") {
+                    $('#blood_donor2').show();
+                    $('#send_msg').show(); 
+
+                } else if (donation_type === "organ" && age <= 25) {
+                    $('#blood_donor3').show();
+                    $('#send_msg').show(); 
+
+                }  else if (donation_type === "both" && age <= 30) {
+                    $('#blood_donor3').show();
+                    $('#send_msg').show(); 
+
+                }  
+                
+                else {
+                    let don_msg = "We are sorry to inform you that, We currently do not have a " +donation_type + " donor. However, We are in the process of receiving more donors." + "Please be patient, Once we get a " + donation_type + " donor onboard, We will contact you immediately, We also advice you to possibly look for other sources." 
+
+                    $('#regret_msg').html(don_msg);
 
                     $('#regrets').show();
-
-            } 
+ 
+                }
                 break;
         }
 
